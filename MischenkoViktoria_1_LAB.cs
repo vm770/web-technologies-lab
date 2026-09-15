93e13c37099a70e0d88119a66754e4cef2c4b8ca
vm770 <viktori090408@gmail.com>
2026-09-15 14:09:26 +0300

    Add files via upload

---
+++ MischenkoViktoria_1_LAB.cs
@@ -0,0 +1,13 @@
+﻿using System;
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+
+  Console.WriteLine("Hello, World!");
+    Console.WriteLine ("Mischenko Viktoria");
+    Console.WriteLine ("11.09.2026"); // Дата виконання
+    Console.ReadKey();
+
+    }
+}
